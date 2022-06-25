5fe6104b9af38d7a6a7dea48cf93bc786e0e1bff
Gomuro <yra3908@gmail.com>
2022-06-25 16:50:43 +0300

    stable version of products

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { CssBaseline } from "@material-ui/core";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import Products from "./components/Products/Products";
+
 import { commerce } from "./lib/commerce";
 
 const App = () => {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+  const [order, setOrder] = useState({});
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const handleDrawerToggle = () => setMobileOpen(!mobileOpen);
+
   return (
-    <>
-      <Products />
-    </>
+    <div>
+      <Products products={products} />
+    </div>
   );
 };
 
--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,61 +1,36 @@
-import React from "react";
-import {
-  Card,
-  CardMedia,
-  CardContent,
-  CardActions,
-  Typography,
-  IconButton,
-  Box,
-} from "@material-ui/core";
-import { AddShoppingCart } from "@material-ui/icons";
-import useStyles from "./styles.js";
+import React from 'react';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/icons';
 
-const Product = ({ product }) => {
+import useStyles from './styles';
+
+const Product = ({ product, onAddToCart }) => {
   const classes = useStyles();
+
+  const handleAddToCart = () => onAddToCart(product.id, 1);
+
   return (
-    <Card className={classes.container}>
-      <CardMedia image={product.image} title={product.name}>
-        <Box>
+    <Card className={classes.root}>
+      <CardMedia className={classes.media} image={product.media.source} title={product.name} />
+      <CardContent>
+        <div className={classes.cardContent}>
           <Typography gutterBottom variant="h5" component="h2">
             {product.name}
           </Typography>
           <Typography gutterBottom variant="h5" component="h2">
-            {product.price}
+            ${product.price.formatted}
           </Typography>
-        </Box>
-      </CardMedia>
+        </div>
+        <Typography dangerouslySetInnerHTML={{ __html: product.description }} variant="body2" color="textSecondary" component="p" />
+      </CardContent>
+      <CardActions disableSpacing className={classes.cardActions}>
+        <IconButton aria-label="Add to Cart" onClick={handleAddToCart}>
+          <AddShoppingCart />
+        </IconButton>
+      </CardActions>
     </Card>
   );
 };
 
 export default Product;
-{
-  /* <div class="container">
-  <div class="card">
-    <img src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8c2hvZXMlMjBuaWtlfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60" alt="" />
-    <div class="card-body">
-      <div class="row">
-        <div class="card-title">
-          <h4>Nike Sneaker</h4>
-          <h3>$120</h3>
-        </div>
-        <div class="view-btn">
-          <a href="">View Details</a>
-        </div>
-      </div>
-      <hr />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi,
-        dignissimos.
-      </p>
-      <div class="btn-group">
-        <div class="btn">
-          <a href="">Buy Now</a>
-        </div>
-        <a href=""> Cancel</a>
-      </div>
-    </div>
-  </div>
-</div> */
-}
+
