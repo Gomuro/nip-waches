71225e30e798f98f0add038af3011f4f3f528f70
Gomuro <yra3908@gmail.com>
2022-06-27 11:00:36 +0300

    final version of products

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,36 +1,72 @@
-import React from 'react';
-import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
-import { AddShoppingCart } from '@material-ui/icons';
+import React from "react";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  IconButton,
+  Box,
+} from "@material-ui/core";
+import {
+  AddShoppingCartOutlined,
+  RemoveShoppingCartOutlined,
+} from "@material-ui/icons";
 
-import useStyles from './styles';
+import useStyles from "./styles";
 
-const Product = ({ product, onAddToCart }) => {
+const Product = ({ product, onAddToCart, handleRemoveToCart }) => {
   const classes = useStyles();
 
   const handleAddToCart = () => onAddToCart(product.id, 1);
 
   return (
     <Card className={classes.root}>
-      <CardMedia className={classes.media} image={product.media.source} title={product.name} />
+      <CardMedia
+        className={classes.media}
+        image={product.media.source}
+        title={product.name}
+      />
       <CardContent>
-        <div className={classes.cardContent}>
-          <Typography gutterBottom variant="h5" component="h2">
-            {product.name}
-          </Typography>
-          <Typography gutterBottom variant="h5" component="h2">
-            ${product.price.formatted}
-          </Typography>
-        </div>
-        <Typography dangerouslySetInnerHTML={{ __html: product.description }} variant="body2" color="textSecondary" component="p" />
+        <Box className={classes.cardContent}>
+          <Box className={classes.row}>
+            <Box className={classes.cardtitle}>
+              <Typography gutterBottom variant="h5" component="h2">
+                {product.name}
+              </Typography>
+              <Typography gutterBottom variant="h5" component="h2">
+                ${product.price.formatted}
+              </Typography>
+            </Box>
+          </Box>
+        </Box>
+        <hr />
+
+        <Typography
+          dangerouslySetInnerHTML={{ __html: product.description }}
+          variant="body1"
+          color="text"
+          component="p"
+        />
+        <Box className={classes.btnGroup}>
+          <Box>
+            <CardActions disableSpacing className={classes.cardActions}>
+              <IconButton aria-label="Add to Cart" onClick={handleAddToCart}>
+                <AddShoppingCartOutlined color="primary" />
+              </IconButton>
+            </CardActions>
+          </Box>
+          <Box>
+            <CardActions disableSpacing className={classes.cardActions}>
+              <IconButton aria-label="Add to Cart" onClick={handleRemoveToCart}>
+                <RemoveShoppingCartOutlined color="error" />
+              </IconButton>
+            </CardActions>
+          </Box>
+        </Box>
       </CardContent>
-      <CardActions disableSpacing className={classes.cardActions}>
-        <IconButton aria-label="Add to Cart" onClick={handleAddToCart}>
-          <AddShoppingCart />
-        </IconButton>
-      </CardActions>
     </Card>
   );
 };
 
 export default Product;
-
--- src/components/Products/Product/styles.js
+++ src/components/Products/Product/styles.js
@@ -1,20 +1,29 @@
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles(() => ({
   root: {
     // maxWidth: 345, original width style
-    maxWidth: '100%',
+    maxWidth: "100%",
+    height: "80vh",
   },
   media: {
-    height: 0,
-    paddingTop: '56.25%', // 16:9
+    width: "100%",
+    height: "300px",
   },
   cardActions: {
-    display: 'flex',
-    justifyContent: 'flex-end',
+    display: "flex",
+    justifyContent: "flex-end",
   },
   cardContent: {
-    display: 'flex',
-    justifyContent: 'space-between',
+    padding: "15px",
+  },
+  row: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: "5px",
+  },
+  btnGroup: {
+    display: "flex",
   },
 }));
