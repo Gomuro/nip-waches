9b258ee1347ffaf5b77a8671bb1759d3dc770e41
Gomuro <yra3908@gmail.com>
2022-07-15 23:30:38 +0300

    Add Product Details

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -1,8 +1,17 @@
-import { Container, Grid, Typography } from "@material-ui/core";
+import {
+  Box,
+  Button,
+  ButtonBase,
+  CardMedia,
+  Container,
+  Grid,
+  Typography,
+} from "@material-ui/core";
 import React, { useContext } from "react";
 import { useParams } from "react-router";
 import StoreContext from "../../context/storeContext";
 import useStyles from "./styles";
+
 const ProductDetails = () => {
   const classes = useStyles();
   const { products } = useContext(StoreContext);
@@ -11,19 +20,57 @@ const ProductDetails = () => {
   if (products === undefined) return <p>Loading...</p>;
 
   return (
-    <div>
+    <>
       {products.map((product) => {
         if (product.id === id) {
+          console.log(
+            "🚀 ~ file: ProductDetails.jsx ~ line 17 ~ products.map ~ product",
+            product
+          );
+
           return (
             <>
-              <Container maxWidth="sm" className={classes.container}>
-                <h1>{product.name}</h1>
-              </Container>
+              <main>
+                <Grid
+                  key={product.id}
+                  style={{ marginTop: "75px" }}
+                  container
+                  direction="row"
+                  justifyContent="center"
+                  alignItems="center"
+                >
+                  <Box>
+                    <CardMedia
+                      style={{ height: "600px", width: "600px" }}
+                      image={product.image.url}
+                      title="lorem ipsum"
+                    />
+                  </Box>
+                  <Box>
+                    <Button variant="outlined" className={classes.iconButton}>
+                      add to cart
+                    </Button>
+                    <Typography variant="h5">{product.name}</Typography>
+                    <Typography variant="h6">
+                      {product.price.formatted_with_symbol}
+                    </Typography>
+
+                    <Typography
+                      dangerouslySetInnerHTML={{
+                        __html: product.description,
+                      }}
+                      variant="body1"
+                      color="textPrimary"
+                      component="p"
+                    />
+                  </Box>
+                </Grid>
+              </main>
             </>
           );
         }
       })}
-    </div>
+    </>
   );
 };
 
